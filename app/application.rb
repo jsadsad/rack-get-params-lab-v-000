2d295834d48e2686d79a37856d72306498e75776
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-04-16 23:00:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,6 +24,7 @@ class Application
       end
     elsif req.path.match(/add/)
       add_item = req.params["item"]
+      binding.pry
       if @@items.include?(add_item)
         @@cart << add_item
         resp.write "added #{add_item}"
